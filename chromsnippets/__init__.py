a21579f3c2858f8ac1c4a21e7235713bc59d1b1e
Thijs <thijsdejong10@gmail.com>
2024-06-03 13:57:52 +0200

    added plot_signal_and_peaks_interactive to init

--- chromsnippets/__init__.py
+++ chromsnippets/__init__.py
@@ -4,3 +4,4 @@ from chromsnippets.analysis import (
     chromatogram_extract_peaks,
 )
 from chromsnippets.data_loader import load_ms, load_fid, load_folder_fid, load_folder_ms
+from chromsnippets.plotting import plot_signal_and_peaks_interactive
